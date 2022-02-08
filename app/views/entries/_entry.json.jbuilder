c4e33964c0d2fd81ae11dc0fdb3888428323683f
Sylvestre Tchangwa <pasytchangwa@gmail.com>
2022-02-09 00:09:20 +0200

    Make chages

--- app/views/entries/_entry.json.jbuilder
+++ app/views/entries/_entry.json.jbuilder
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 json.extract! entry, :id, :meal_type, :calories, :proteins, :carbohydrates, :fats, :created_at, :updated_at
 json.url entry_url(entry, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,19 +12,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_164403) do
-
+ActiveRecord::Schema.define(version: 20_220_205_164_403) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "entries", force: :cascade do |t|
-    t.string "meal_type"
-    t.integer "calories"
-    t.integer "proteins"
-    t.integer "carbohydrates"
-    t.integer "fats"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'entries', force: :cascade do |t|
+    t.string 'meal_type'
+    t.integer 'calories'
+    t.integer 'proteins'
+    t.integer 'carbohydrates'
+    t.integer 'fats'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
